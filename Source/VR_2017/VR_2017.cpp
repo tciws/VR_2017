b5549e656189508dd5d712d35220e46cb07f8405
DESKTOP-FG58J0M\remu <remu.0197@gmail.com>
2017-04-17 00:46:27 +0900

    first commit
    first commit

---
+++ Source/VR_2017/VR_2017.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "VR_2017.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VR_2017, "VR_2017" );
